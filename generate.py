04c45292b14cc2558ffbc35379cae6f74c7862e1
Balint <balint@evelinas-air.lan>
2025-02-02 13:06:10 +0100

    Changing back build directory, forcing rebuild

--- generate.py
+++ generate.py
@@ -10,7 +10,7 @@ post_template = env.get_template('post.html')
 index_template = env.get_template('index.html')
 
 
-BUILD_DIR = 'docs'
+BUILD_DIR = 'build'
 
 def render_template(template, **kwargs):
     base_html_start = template.render(kwargs, content="", title="Blog")
